c952bfa1c90e9f72b52668c2efaabd0853430b91
voicanthi <hoang.nguyenxuan.developer@gmail.com>
2017-10-08 20:56:33 +0700

    init project

---
+++ toeic-web-logic/src/main/java/vn/myclass/web/logic/common/WebConstant.java
@@ -0,0 +1,8 @@
+package vn.myclass.web.logic.common;
+
+/**
+ * Created by xuanh on 09/24/17.
+ */
+public class WebConstant {
+
+}
---
+++ toeic-web/src/main/webapp/index.jsp
@@ -0,0 +1,13 @@
+<html>
+
+<head>
+
+</head>
+
+<body>
+
+<h1>TOEIC ONLINE</h1>
+
+</body>
+
+</html>
